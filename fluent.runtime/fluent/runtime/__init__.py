django-ftl/python-fluent

Compiler implementation for FluentBundle

fluent.runtime/fluent/runtime/__init__.py
@@ -1,19 +1,23 @@
 from __future__ import absolute_import, unicode_literals
 
+from collections import OrderedDict
+
 import babel
 import babel.numbers
 import babel.plural
 
 from fluent.syntax import FluentParser
-from fluent.syntax.ast import Message, Term
+from fluent.syntax.ast import Junk, Message, Term
 
 from .builtins import BUILTINS
+from .compiler import compile_messages
+from .errors import FluentDuplicateMessageId, FluentJunkFound
 from .prepare import Compiler
-from .resolver import ResolverEnvironment, CurrentEnvironment
+from .resolver import CurrentEnvironment, ResolverEnvironment
 from .utils import ATTRIBUTE_SEPARATOR, TERM_SIGIL, ast_to_id, native_to_fluent
 
 
-class FluentBundle(object):
+class FluentBundleBase(object):
     """
     Message contexts are single-language stores of translations.  They are
     responsible for parsing translation resources in the Fluent syntax and can
@@ -33,26 +37,59 @@ class FluentBundle(object):
             _functions.update(functions)
         self._functions = _functions
         self._use_isolating = use_isolating
-        self._messages_and_terms = {}
-        self._compiled = {}
-        self._compiler = Compiler(use_isolating=use_isolating)
+        self._messages_and_terms = OrderedDict()
+        self._parsing_issues = []
         self._babel_locale = self._get_babel_locale()
         self._plural_form = babel.plural.to_python(self._babel_locale.plural_form)
 
     def add_messages(self, source):
         parser = FluentParser()
         resource = parser.parse(source)
-        # TODO - warn/error about duplicates
         for item in resource.body:
             if isinstance(item, (Message, Term)):
                 full_id = ast_to_id(item)
-                if full_id not in self._messages_and_terms:
+                if full_id in self._messages_and_terms:
+                    self._parsing_issues.append((full_id, FluentDuplicateMessageId(
+                        "Additional definition for '{0}' discarded.".format(full_id))))
+                else:
                     self._messages_and_terms[full_id] = item
+            elif isinstance(item, Junk):
+                self._parsing_issues.append(
+                    (None, FluentJunkFound("Junk found: " +
+                                           '; '.join(a.message for a in item.annotations),
+                                           item.annotations)))
 
     def has_message(self, message_id):
         if message_id.startswith(TERM_SIGIL) or ATTRIBUTE_SEPARATOR in message_id:
             return False
         return message_id in self._messages_and_terms
+
+    def _get_babel_locale(self):
+        for l in self.locales:
+            try:
+                return babel.Locale.parse(l.replace('-', '_'))
+            except babel.UnknownLocaleError:
+                continue
+        # TODO - log error
+        return babel.Locale.default()
+
+    def format(self, message_id, args=None):
+        raise NotImplementedError()
+
+    def check_messages(self):
+        """
+        Check messages for errors and return as a list of two tuples:
+           (message ID or None, exception object)
+        """
+        raise NotImplementedError()
+
+
+class InterpretingFluentBundle(FluentBundleBase):
+
+    def __init__(self, locales, functions=None, use_isolating=True):
+        super(InterpretingFluentBundle, self).__init__(locales, functions=functions, use_isolating=use_isolating)
+        self._compiled = {}
+        self._compiler = Compiler(use_isolating=use_isolating)
 
     def lookup(self, full_id):
         if full_id not in self._compiled:
@@ -83,11 +120,55 @@ class FluentBundle(object):
                                   errors=errors)
         return [resolve(env), errors]
 
-    def _get_babel_locale(self):
-        for l in self.locales:
-            try:
-                return babel.Locale.parse(l.replace('-', '_'))
-            except babel.UnknownLocaleError:
-                continue
-        # TODO - log error
-        return babel.Locale.default()
+    def check_messages(self):
+        return self._parsing_issues[:]
+
+
+class CompilingFluentBundle(FluentBundleBase):
+    def __init__(self, *args, **kwargs):
+        super(CompilingFluentBundle, self).__init__(*args, **kwargs)
+        self._mark_dirty()
+
+    def _mark_dirty(self):
+        self._is_dirty = True
+        # Clear out old compilation errors, they might not apply if we
+        # re-compile:
+        self._compilation_errors = []
+        self.format = self._compile_and_format
+
+    def _mark_clean(self):
+        self._is_dirty = False
+        self.format = self._format
+
+    def add_messages(self, source):
+        super(CompilingFluentBundle, self).add_messages(source)
+        self._mark_dirty()
+
+    def _compile(self):
+        self._compiled_messages, self._compilation_errors = compile_messages(
+            self._messages_and_terms,
+            self._babel_locale,
+            use_isolating=self._use_isolating,
+            functions=self._functions)
+        self._mark_clean()
+
+    # 'format' is the hot path for many scenarios, so we try to optimize it. To
+    # avoid having to check '_is_dirty' inside 'format', we switch 'format' from
+    # '_compile_and_format' to '_format' when compilation is done. This gives us
+    # about 10% improvement for the simplest (but most common) case of an
+    # entirely static string.
+    def _compile_and_format(self, message_id, args=None):
+        self._compile()
+        return self._format(message_id, args)
+
+    def _format(self, message_id, args=None):
+        errors = []
+        return self._compiled_messages[message_id](args, errors), errors
+
+    def check_messages(self):
+        if self._is_dirty:
+            self._compile()
+        return self._parsing_issues + self._compilation_errors
+
+
+FluentBundle = InterpretingFluentBundle

django-ftl/fluent-compiler

fluent.runtime/fluent/runtime/__init__.py
@@ -1,19 +1,23 @@
 from __future__ import absolute_import, unicode_literals
 
+from collections import OrderedDict
+
 import babel
 import babel.numbers
 import babel.plural
 
 from fluent.syntax import FluentParser
-from fluent.syntax.ast import Message, Term
+from fluent.syntax.ast import Junk, Message, Term
 
 from .builtins import BUILTINS
+from .compiler import compile_messages
+from .errors import FluentDuplicateMessageId, FluentJunkFound
 from .prepare import Compiler
-from .resolver import ResolverEnvironment, CurrentEnvironment
+from .resolver import CurrentEnvironment, ResolverEnvironment
 from .utils import ATTRIBUTE_SEPARATOR, TERM_SIGIL, ast_to_id, native_to_fluent
 
 
-class FluentBundle(object):
+class FluentBundleBase(object):
     """
     Message contexts are single-language stores of translations.  They are
     responsible for parsing translation resources in the Fluent syntax and can
@@ -33,26 +37,59 @@ class FluentBundle(object):
             _functions.update(functions)
         self._functions = _functions
         self.use_isolating = use_isolating
-        self._messages_and_terms = {}
-        self._compiled = {}
-        self._compiler = Compiler()
+        self._messages_and_terms = OrderedDict()
+        self._parsing_issues = []
         self._babel_locale = self._get_babel_locale()
         self._plural_form = babel.plural.to_python(self._babel_locale.plural_form)
 
     def add_messages(self, source):
         parser = FluentParser()
         resource = parser.parse(source)
-        # TODO - warn/error about duplicates
         for item in resource.body:
             if isinstance(item, (Message, Term)):
                 full_id = ast_to_id(item)
-                if full_id not in self._messages_and_terms:
+                if full_id in self._messages_and_terms:
+                    self._parsing_issues.append((full_id, FluentDuplicateMessageId(
+                        "Additional definition for '{0}' discarded.".format(full_id))))
+                else:
                     self._messages_and_terms[full_id] = item
+            elif isinstance(item, Junk):
+                self._parsing_issues.append(
+                    (None, FluentJunkFound("Junk found: " +
+                                           '; '.join(a.message for a in item.annotations),
+                                           item.annotations)))
 
     def has_message(self, message_id):
         if message_id.startswith(TERM_SIGIL) or ATTRIBUTE_SEPARATOR in message_id:
             return False
         return message_id in self._messages_and_terms
+
+    def _get_babel_locale(self):
+        for l in self.locales:
+            try:
+                return babel.Locale.parse(l.replace('-', '_'))
+            except babel.UnknownLocaleError:
+                continue
+        # TODO - log error
+        return babel.Locale.default()
+
+    def format(self, message_id, args=None):
+        raise NotImplementedError()
+
+    def check_messages(self):
+        """
+        Check messages for errors and return as a list of two tuples:
+           (message ID or None, exception object)
+        """
+        raise NotImplementedError()
+
+
+class InterpretingFluentBundle(FluentBundleBase):
+
+    def __init__(self, locales, functions=None, use_isolating=True):
+        super(InterpretingFluentBundle, self).__init__(locales, functions=functions, use_isolating=use_isolating)
+        self._compiled = {}
+        self._compiler = Compiler()
 
     def lookup(self, full_id):
         if full_id not in self._compiled:
@@ -83,11 +120,55 @@ class FluentBundle(object):
                                   errors=errors)
         return [resolve(env), errors]
 
-    def _get_babel_locale(self):
-        for l in self.locales:
-            try:
-                return babel.Locale.parse(l.replace('-', '_'))
-            except babel.UnknownLocaleError:
-                continue
-        # TODO - log error
-        return babel.Locale.default()
+    def check_messages(self):
+        return self._parsing_issues[:]
+
+
+class CompilingFluentBundle(FluentBundleBase):
+    def __init__(self, *args, **kwargs):
+        super(CompilingFluentBundle, self).__init__(*args, **kwargs)
+        self._mark_dirty()
+
+    def _mark_dirty(self):
+        self._is_dirty = True
+        # Clear out old compilation errors, they might not apply if we
+        # re-compile:
+        self._compilation_errors = []
+        self.format = self._compile_and_format
+
+    def _mark_clean(self):
+        self._is_dirty = False
+        self.format = self._format
+
+    def add_messages(self, source):
+        super(CompilingFluentBundle, self).add_messages(source)
+        self._mark_dirty()
+
+    def _compile(self):
+        self._compiled_messages, self._compilation_errors = compile_messages(
+            self._messages_and_terms,
+            self._babel_locale,
+            use_isolating=self.use_isolating,
+            functions=self._functions)
+        self._mark_clean()
+
+    # 'format' is the hot path for many scenarios, so we try to optimize it. To
+    # avoid having to check '_is_dirty' inside 'format', we switch 'format' from
+    # '_compile_and_format' to '_format' when compilation is done. This gives us
+    # about 10% improvement for the simplest (but most common) case of an
+    # entirely static string.
+    def _compile_and_format(self, message_id, args=None):
+        self._compile()
+        return self._format(message_id, args)
+
+    def _format(self, message_id, args=None):
+        errors = []
+        return self._compiled_messages[message_id](args, errors), errors
+
+    def check_messages(self):
+        if self._is_dirty:
+            self._compile()
+        return self._parsing_issues + self._compilation_errors
+
+
+FluentBundle = InterpretingFluentBundle