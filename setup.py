90f0474d6d2b43b622dc57bc2fb161717fd55bdb
Luke Plant <L.Plant.98@cantab.net>
2020-03-19 20:24:39 +0300

    Working setup.py and tox.ini

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
 import sys
 if sys.version_info < (3, 4):
@@ -7,9 +7,6 @@ if sys.version_info < (3, 4):
 else:
     # functools.singledispatch is in stdlib from Python 3.4 onwards.
     old_python_requires = []
-
-tests_requires = ['ast_decompiler>=0.3.2', 'beautifulsoup4>=4.7.1', 'hypothesis>=4.9.0',
-                  'Markdown>=3.0.1', 'MarkupSafe>=1.1.1']
 
 setup(name='fluent_compiler',
       version='0.1',
@@ -28,7 +25,7 @@ setup(name='fluent_compiler',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
       ],
-      packages=['fluent_compiler'],
+      packages=find_packages('src'),
       package_dir={'': 'src'},
       # These should also be duplicated in tox.ini and ../.travis.yml
       install_requires=[
@@ -38,9 +35,4 @@ setup(name='fluent_compiler',
           'pytz',
           'six',
       ] + old_python_requires,
-      test_suite='tests',
-      tests_require=tests_requires,  # for 'setup.py test'
-      extras_require={
-          'develop': tests_requires,  # for 'pip install fluent_compiler[develop]'
-      },
       )
